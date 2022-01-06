a6549084172eb4e32a79781848d580c87874bfac
Marco Neumann <marco@crepererum.net>
2022-01-06 11:25:59 +0100

    docs: add more references

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -1,3 +1,9 @@
+//! The Apache Kafka protocol.
+//!
+//! # References
+//! - <https://github.com/edenhill/librdkafka/blob/2b76b65212e5efda213961d5f84e565038036270/src/rdkafka_feature.c#L52-L212>
+//! - <https://kafka.apache.org/protocol>
+//! - <https://kafka.apache.org/documentation>
 pub mod api_key;
 pub mod api_version;
 pub mod error;
