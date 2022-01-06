34f7269d29d2f35e03b3e3d80d8634c3c6b59a53
Marco Neumann <marco@crepererum.net>
2022-01-06 09:47:13 +0100

    fix: harden rdkafka consume adapter

--- tests/rdkafka_helper.rs
+++ tests/rdkafka_helper.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use futures::{StreamExt, TryStreamExt};
 use minikafka::record::Record;
 use rdkafka::{
@@ -62,41 +64,62 @@ pub async fn consume(
     partition_index: i32,
     n: usize,
 ) -> Vec<Record> {
-    // create client
-    let mut cfg = ClientConfig::new();
-    cfg.set("bootstrap.servers", connection);
-    cfg.set("message.timeout.ms", "5000");
-    cfg.set("group.id", "foo");
-    cfg.set("auto.offset.reset", "smallest");
-    let client: StreamConsumer<_> = cfg.create().unwrap();
+    tokio::time::timeout(Duration::from_secs(10), async move {
+        loop {
+            // create client
+            let mut cfg = ClientConfig::new();
+            cfg.set("bootstrap.servers", connection);
+            cfg.set("message.timeout.ms", "5000");
+            cfg.set("group.id", "foo");
+            cfg.set("auto.offset.reset", "smallest");
+            let client: StreamConsumer<_> = cfg.create().unwrap();
 
-    let mut assignment = TopicPartitionList::new();
-    assignment.add_partition(topic_name, partition_index);
-    client.assign(&assignment).unwrap();
+            let mut assignment = TopicPartitionList::new();
+            assignment.add_partition(topic_name, partition_index);
+            client.assign(&assignment).unwrap();
 
-    client
-        .stream()
-        .take(n)
-        .map_ok(|msg| Record {
-            key: msg.key().map(|k| k.to_vec()).unwrap_or_default(),
-            value: msg.payload().map(|v| v.to_vec()).unwrap_or_default(),
-            headers: msg
-                .headers()
-                .map(|headers| {
-                    (0..headers.count())
-                        .map(|i| {
-                            let (k, v) = headers.get(i).unwrap();
-                            (k.to_owned(), v.to_vec())
+            let res = client
+                .stream()
+                .take(n)
+                .map_ok(|msg| Record {
+                    key: msg.key().map(|k| k.to_vec()).unwrap_or_default(),
+                    value: msg.payload().map(|v| v.to_vec()).unwrap_or_default(),
+                    headers: msg
+                        .headers()
+                        .map(|headers| {
+                            (0..headers.count())
+                                .map(|i| {
+                                    let (k, v) = headers.get(i).unwrap();
+                                    (k.to_owned(), v.to_vec())
+                                })
+                                .collect()
                         })
-                        .collect()
+                        .unwrap_or_default(),
+                    timestamp: OffsetDateTime::from_unix_timestamp_nanos(
+                        msg.timestamp().to_millis().unwrap_or_default() as i128 * 1_000_000,
+                    )
+                    .unwrap(),
                 })
-                .unwrap_or_default(),
-            timestamp: OffsetDateTime::from_unix_timestamp_nanos(
-                msg.timestamp().to_millis().unwrap_or_default() as i128 * 1_000_000,
-            )
-            .unwrap(),
-        })
-        .try_collect()
-        .await
-        .unwrap()
+                .try_collect()
+                .await;
+
+            match res {
+                Ok(records) => {
+                    return records;
+                }
+                Err(e) => {
+                    // this might happen on a fresh rdkafka node
+                    // (e.g. "KafkaError (Message consumption error: NotCoordinator (Broker: Not coordinator))")
+                    println!(
+                        "Encountered rdkafka error while consuming, try again: {:?}",
+                        e
+                    );
+                    tokio::time::sleep(Duration::from_millis(100)).await;
+                    continue;
+                }
+            }
+        }
+    })
+    .await
+    .unwrap()
 }
