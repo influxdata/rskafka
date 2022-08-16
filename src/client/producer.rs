32c34ec9c2b2c6b001920442e52f9713ae770875
Marco Neumann <marco@crepererum.net>
2022-08-16 16:25:41 +0200

    fix: address deprecation warnings

--- src/client/producer.rs
+++ src/client/producer.rs
@@ -608,9 +608,9 @@ mod tests {
 
             let mut futures = FuturesOrdered::new();
 
-            futures.push(producer.produce(record.clone()));
-            futures.push(producer.produce(record.clone()));
-            futures.push(producer.produce(record.clone()));
+            futures.push_back(producer.produce(record.clone()));
+            futures.push_back(producer.produce(record.clone()));
+            futures.push_back(producer.produce(record.clone()));
 
             let assert_ok = |a: Result<Option<Result<_, _>>, _>, expected: i64| {
                 let offset = a
