3248dd6bc76f1a31377994dbc96c0580ac4d0619
Marco Neumann <marco@crepererum.net>
2022-08-23 11:21:03 +0200

    ci: check that versions are in-sync

---
+++ .circleci/check_versions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Checks that container versions in CI and dev tooling like docker compose files are in-sync
+
+set -euo pipefail
+
+readonly SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+readonly DEPS=(redpanda kafka zookeeper)
+
+for dependency in "${DEPS[@]}"; do
+    echo "========================================"
+    echo "Checking $dependency"
+    versions="$(find . \( -name target -prune \) -o \( -name .git -prune \) -o \( -name coverage -prune \) -o \( -name corpus -prune \) -o -type f -exec grep image: {} \; | grep "$dependency" | sed -e 's/^.*image:.*://' | sort -u)"
+    echo "Found:"
+    echo "$versions"
+    n="$(echo "$versions" | wc -l)"
+    if [[ $n != 1 ]]; then
+        echo "Found $n different versions!"
+        exit 1
+    fi
+done
